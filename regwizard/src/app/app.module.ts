9fedb75449c64ea7e5e11804ef21b192338251b3
Victor Khotin <vikhotin@yandex.ru>
2023-11-07 21:23:18 +0300

    add style (bootstrap)

--- regwizard/src/app/app.module.ts
+++ regwizard/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { RegFormComponent } from './reg-form/reg-form.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule
